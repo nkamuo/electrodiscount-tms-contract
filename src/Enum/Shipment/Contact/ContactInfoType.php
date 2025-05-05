24dd652c182c1757104badae3d054d87dbeb8f77
Callistus Nkamuo <callistusnkamuo@gmail.com>
2025-05-05 13:45:20 +0100

    Added ContactInfoType enum in the project

---
+++ src/Enum/Shipment/Contact/ContactInfoType.php
@@ -0,0 +1,9 @@
+<?php
+namespace Nkamuo\Electrodiscount\TMS\Contract\Enum\Shipment\Contact;
+
+
+enum ContactInfoType: string{
+    case INDIVIDUAL = 'INDIVIDUAL';
+    case BUSINESS = 'BUSINESS';
+    // case OTHER = 'OTHER';
+}
