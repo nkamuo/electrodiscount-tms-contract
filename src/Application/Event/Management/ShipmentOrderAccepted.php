bd3e58bb27c83df056ceea493ce0172b931f2477
Callistus Nkamuo <callistusnkamuo@gmail.com>
2025-05-08 23:25:11 +0100

    Added Management Events to Signal Shipment Accepted or Rejected

---
+++ src/Application/Event/Management/ShipmentOrderAccepted.php
@@ -0,0 +1,13 @@
+<?php
+namespace Nkamuo\Electrodiscount\TMS\Contract\Application\Event\Management;
+
+use Nkamuo\Electrodiscount\TMS\Contract\Application\Event\ShipmentAsyncEvent;
+use Symfony\Component\Uid\Ulid;
+
+readonly class ShipmentOrderAccepted implements ShipmentAsyncEvent{
+    public function __construct(
+        public Ulid $orderId,
+        public ?string $reason = null,
+        public ?string $note = null,
+    ){}
+}
