6175ad6539e5d81425fb8d9a5ecab8b41090f85d
Callistus Nkamuo <callistusnkamuo@gmail.com>
2025-05-20 15:57:26 +0100

    Added Commands  and Events to Delete Shipment Quote

--- src/Application/Command/Quote/RemoveShipmentQuote.php
+++ src/Application/Command/Quote/RemoveShipmentQuote.php
@@ -1,13 +1,12 @@
 <?php
 namespace Nkamuo\Electrodiscount\TMS\Contract\Application\Command\Quote;
 
-use Nkamuo\Electrodiscount\TMS\Contract\Dto\Shipment\Quote\QuoteDTO;
 use Symfony\Component\Uid\Ulid;
 
-final readonly class CreateShipmentQuote{
+final readonly class RemoveShipmentQuote{
     public function __construct(
         public Ulid $shipmentId,
-        public QuoteDTO $quote,
+        public Ulid $quoteId,
         public ?string $reason = null,
         public ?string $note = null,
     ){}
--- src/Application/Event/Quote/ShipmentQuoteRemoved.php
+++ src/Application/Event/Quote/ShipmentQuoteRemoved.php
@@ -1,17 +1,13 @@
 <?php
-
 namespace Nkamuo\Electrodiscount\TMS\Contract\Application\Event\Quote;
 
-use Nkamuo\Electrodiscount\TMS\Contract\Dto\Shipment\Quote\QuoteDTO;
 use Symfony\Component\Uid\Ulid;
 
-final readonly class ShipmentQuoteCreated
-{
+final readonly class ShipmentQuoteRemoved{
     public function __construct(
-        public Ulid $quoteId,
         public Ulid $shipmentId,
-        public QuoteDTO $quote,
+        public Ulid $quoteId,
         public ?string $reason = null,
         public ?string $note = null,
-    ) {}
+    ){}
 }
