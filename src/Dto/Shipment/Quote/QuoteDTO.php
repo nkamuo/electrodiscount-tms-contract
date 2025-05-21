2621cdb10112166775db7a51701ae7dcf8b190dd
Callistus Nkamuo <callistusnkamuo@gmail.com>
2025-05-21 16:24:35 +0100

    Removed unneccessary fields from the QuoteDTO and AdjustmentDTO classes

--- src/Dto/Shipment/Quote/QuoteDTO.php
+++ src/Dto/Shipment/Quote/QuoteDTO.php
@@ -8,47 +8,32 @@ use DateTimeImmutable;
 class QuoteDTO
 {
     public function __construct(
-        public readonly ?int $id,
-        public readonly ?int $shipmentId,
-        public readonly ?string $currency,
-        public readonly ?int $amount,
-        public readonly ?LocalDate $date,
-        public readonly ?string $note,
-        public readonly ?string $description,
-        public readonly ?DateTimeImmutable $createdAt,
+        public readonly ?string $currency = null,
+        public readonly ?int $amount = null,
+        public readonly ?LocalDate $date = null,
+        public readonly ?string $note = null,
+        public readonly ?string $description = null,
         /** @var AdjustmentDTO[] */
-        public readonly array $adjustments,
-        public readonly ?int $adjustmentsTotal,
-        public readonly ?int $total,
-        public readonly ?string $status,
+        public readonly ?array $adjustments = null,
+        public readonly ?string $status = null,
     ) {}
 
     public function copyWith(
-        ?int $id = null,
-        ?int $shipmentId = null,
         ?string $currency = null,
         ?int $amount = null,
         ?LocalDate $date = null,
         ?string $note = null,
         ?string $description = null,
-        ?DateTimeImmutable $createdAt = null,
         ?array $adjustments = null,
-        ?int $adjustmentsTotal = null,
-        ?int $total = null,
         ?string $status = null,
     ): self {
         return new self(
-            id: $id ?? $this->id,
-            shipmentId: $shipmentId ?? $this->shipmentId,
             currency: $currency ?? $this->currency,
             amount: $amount ?? $this->amount,
             date: $date ?? $this->date,
             note: $note ?? $this->note,
             description: $description ?? $this->description,
-            createdAt: $createdAt ?? $this->createdAt,
             adjustments: $adjustments ?? $this->adjustments,
-            adjustmentsTotal: $adjustmentsTotal ?? $this->adjustmentsTotal,
-            total: $total ?? $this->total,
             status: $status ?? $this->status,
         );
     }
