ccf90a9353cec2324f648b6b6da6952c6a09b709
Callistus Nkamuo <callistusnkamuo@gmail.com>
2025-06-10 19:58:58 +0100

    Added Command and Event to Remove Channels

---
+++ src/Application/Event/Channel/ChannelRemoved.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Nkamuo\Electrodiscount\TMS\Contract\Application\Event\Channel;
+
+use Nkamuo\Electrodiscount\TMS\Contract\Application\Event\Business\BusinessClientAsyncEvent;
+use Nkamuo\Electrodiscount\TMS\Contract\Application\Event\Business\BusinessClientAsyncEventTrait;
+use Symfony\Component\Uid\Ulid;
+use Symfony\Component\Validator\Constraints as Assert;
+
+readonly class ChannelRemoved implements BusinessClientAsyncEvent
+{
+    use BusinessClientAsyncEventTrait;
+    
+    public function __construct(
+
+        #[Assert\NotNull()]
+        public ?Ulid $channelId = null,
+
+        public ?Ulid $clientId = null,
+
+        #[Assert\Length(max: 225)]
+        public ?string $reason = null,
+
+        #[Assert\Length(max: 500)]
+        public ?string $note = null,
+    ) {}
+}
