8332a17b34d65873a791c87d36a52522b055c1a3
Callistus Nkamuo <callistusnkamuo@gmail.com>
2025-06-10 20:26:38 +0100

    Added Command and Event to Update Channel Details

---
+++ src/Application/Command/Channel/UpdateChannelSubtitle.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Nkamuo\Electrodiscount\TMS\Contract\Application\Command\Channel;
+
+use Symfony\Component\Uid\Ulid;
+use Symfony\Component\Validator\Constraints as Assert;
+
+readonly class UpdateChannelSubtitle
+{
+    public function __construct(
+
+        #[Assert\NotNull()]
+        public Ulid $channelId,
+
+        #[Assert\Length(max: 225)]
+        public ?string $subtitle = null,
+
+        #[Assert\Length(max: 225)]
+        public ?string $reason = null,
+
+        #[Assert\Length(max: 500)]
+        public ?string $note = null,
+    ) {}
+}
---
+++ src/Application/Event/Channel/ChannelCredentialsUpdated.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Nkamuo\Electrodiscount\TMS\Contract\Application\Command\Channel;
+
+use Nkamuo\Electrodiscount\TMS\Contract\Application\Event\Business\BusinessClientAsyncEvent;
+use Nkamuo\Electrodiscount\TMS\Contract\Application\Event\Business\BusinessClientAsyncEventTrait;
+use Symfony\Component\Uid\Ulid;
+use Symfony\Component\Validator\Constraints as Assert;
+
+readonly class ChannelCredentialsUpdated implements BusinessClientAsyncEvent
+{
+    use BusinessClientAsyncEventTrait;
+    public function __construct(
+        #[Assert\NotNull()]
+        public Ulid $channelId,
+        
+        #[Assert\NotNull()]
+        public ?Ulid $clientId = null,
+
+        #[Assert\NotNull()]
+        public array $credentials,
+
+
+        #[Assert\Length(max: 225)]
+        public ?string $reason = null,
+
+        #[Assert\Length(max: 500)]
+        public ?string $note = null,
+    ) {}
+}
